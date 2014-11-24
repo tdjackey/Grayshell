8e5e092652e8770c5c8bef5603b6710b60bb4691
tdjackey <tdjackey@gmail.com>
2014-11-24 23:06:44 +0800

    tiny change

--- Example2/meet.c
+++ Example2/meet.c
@@ -1,14 +1,14 @@
 
 #include<string.h>
-void greeting(char *temp1, char *temp2){
+greeting(char *temp1, char *temp2){
 	char name[400];
 	strcpy(name, temp2);
-	printf("Hello %s %s \n ", temp1, name);
+	printf("Hello %s %s\n ", temp1, name);
 
 }
 main(int argc, char * argv[]){
 	greeting(argv[1], argv[2]);
-	printf("Bye %s %s \n", argv[1], argv[2]);
+	printf("Bye %s %s\n", argv[1], argv[2]);
 	
 }
 //perl -e 'print "A" x 600'
